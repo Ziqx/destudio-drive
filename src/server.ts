32bd01dd0cd13aeacb22f5b216bc1aaf7c0b77df
fathah-work <abdulfathahkodag@gmail.com>
2024-08-17 11:50:51 +0400

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import NextDriveServer from 'nextdrive';
+import { config } from 'dotenv';
+import path from 'path';
+
+config({ path: path.resolve(__dirname, '../.env') });
+
+const server = new NextDriveServer(
+    {
+        port: 4003,
+        apiKey: process.env.API_KEY!,
+        options: {
+            cors: true
+        }
+    });
+server.start();
